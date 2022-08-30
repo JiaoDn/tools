21429f812840c2d283468cb847f468fbb724653a
dian_jiao <jiao.dian@asiainfo-sec.com>
2022-08-30 23:35:09 +0800

    passwd encrypt and check

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginParams struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func GenerateEncryptPasswd(Password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	encodePWD := string(hash)
+	fmt.Println("oricode:" + Password)
+	fmt.Println("encode:" + encodePWD)
+	return encodePWD, nil
 }
 
 func AuthMiddleware() gin.HandlerFunc {
@@ -21,6 +34,16 @@ func AuthMiddleware() gin.HandlerFunc {
 
 		fmt.Println(loginParams.Username)
 		fmt.Println(loginParams.Password)
+		enPasswd, err := GenerateEncryptPasswd("Mac8.678")
+		if err != nil {
+			fmt.Println("GenerateEncryptPasswd error")
+		}
+
+		if err := bcrypt.CompareHashAndPassword([]byte(enPasswd), []byte(loginParams.Password)); err != nil {
+			fmt.Println("pwd wrong")
+		} else {
+			fmt.Println("pwd ok")
+		}
 
 	}
 
