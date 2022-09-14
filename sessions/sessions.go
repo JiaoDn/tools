0792b0a4e8206562ed7cfbf77f3e1a51425694b4
dian_jiao <jiao.dian@asiainfo-sec.com>
2022-09-14 23:57:46 +0800

    session

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,9 +1,11 @@
 package sessions
 
 import (
+	"container/list"
 	"crypto/rand"
 	"encoding/base64"
 	"sync"
+	"time"
 )
 
 type Manager struct {
@@ -22,4 +24,51 @@ func (manager *Manager) sessionId() string {
 		return ""
 	}
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+type SessionStore struct {
+	sessionId       string                      // session id唯一标示
+	recentlyVisited time.Time                   // 最后访问时间
+	value           map[interface{}]interface{} // session里面存储的值
+}
+
+func (st *SessionStore) Set(key, value interface{}) error {
+	st.value[key] = value
+
+	return nil
+}
+
+func (st *SessionStore) Get(key interface{}) interface{} {
+
+	if v, ok := st.value[key]; ok {
+		return v
+	} else {
+		return nil
+	}
+}
+
+func (st *SessionStore) Delete(key interface{}) error {
+	delete(st.value, key)
+
+	return nil
+}
+
+func (st *SessionStore) SessionID() string {
+	return st.sessionId
+}
+
+type LocalStore struct {
+	lock     sync.Mutex               // 用来锁
+	sessions map[string]*list.Element // 用来存储在内存
+	list     *list.List               // 用来做 gc
+}
+
+func (ls *LocalStore) SessionInit(sessionId string) (interface{}, error) {
+	ls.lock.Lock()
+	defer ls.lock.Unlock()
+	v := make(map[interface{}]interface{}, 0)
+	newsess := &SessionStore{sessionId: sessionId, recentlyVisited: time.Now(), value: v}
+	element := ls.list.PushFront(newsess)
+	ls.sessions[sessionId] = element
+	return newsess, nil
 }
